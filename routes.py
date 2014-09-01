a91fd50ddb2d7b3f226faa49d534b8f90521ebd9
Steven Pousty <spousty@redhat.com>
2014-08-31 19:01:50 -0700

    now with Insults

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 __author__ = 'spousty'
 
 from flask import Flask
+import insulter
 
 app = Flask(__name__)
 # Keeps Flask from swallowing error messages
@@ -8,7 +9,11 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 
 @app.route("/")
 def insult():
-    return "Hello, code monkey!"
+    return insulter.insult()
+
+@app.route("/<name>")
+def insult_name(name):
+    return insulter.named_insult(name)
 
 if __name__ == "__main__":
     app.run()
