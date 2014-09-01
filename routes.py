c62a1517f965b4744052da831695f2f0113971ee
Steven Pousty <spousty@redhat.com>
2014-08-31 18:19:09 -0700

    Deploy

---
+++ routes.py
@@ -0,0 +1,14 @@
+__author__ = 'spousty'
+
+from flask import Flask
+
+app = Flask(__name__)
+# Keeps Flask from swallowing error messages
+app.config['PROPAGATE_EXCEPTIONS'] = True
+
+@app.route("/")
+def insult():
+    return "Hello, code monkey!"
+
+if __name__ == "__main__":
+    app.run()
